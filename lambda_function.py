feb79c6105194262cd770a5c097ef3d32984fe43
DESKTOP-GID09QK\yoshi <hiro.engineer0690@gmail.com>
2022-10-19 01:30:08 +0900

    [fix] バグ修正

--- lambda_function.py
+++ lambda_function.py
@@ -25,25 +25,24 @@ def lambda_handler(event, context):
 
 
     @LINE_HANDLER.add(FollowEvent)
-    def handle_follow(event):
-        profile = LINE_BOT_API.get_profile(event.source.user_id)
+    def handle_follow(line_follow_event):
+        profile = LINE_BOT_API.get_profile(line_follow_event.source.user_id)
         logger.info(profile)
 
-        userName = profile["displayName"]
-        LINE_BOT_API.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'{userName}さん、こんにちは！\nぼくのなまえは「まもるくん」。\n「スタート」と送信すると問題がはじまるよ!!'))
+        # userName = profile["displayName"]
+        userId = profile["userId"]
+        LINE_BOT_API.push_message(userId, TextSendMessage(text='こんにちは！\nぼくのなまえは「まもるくん」。\n「スタート」と送信すると問題がはじまるよ!!'))
 
 
     @LINE_HANDLER.add(MessageEvent, message=TextMessage)
-    def on_message(line_event):
+    def on_message(line_reply_event):
         # profile = LINE_BOT_API.get_profile(line_event.source.user_id)
         # logger.info(profile)
 
-        message = line_event.message.text.lower()
+        message = line_reply_event.message.text.lower()
         answerlist = ["よい", "貸す", "保存する"]
         if message == 'スタート':
-            LINE_BOT_API.reply_message(line_event.reply_token,
+            LINE_BOT_API.reply_message(line_reply_event.reply_token,
                 TextSendMessage(text='第1問目\n知らない人から送られてきたURLはクリックしてもよい？',
                         quick_reply=QuickReply(items=[
                             QuickReplyButton(action=PostbackAction(label="よい", data="よい", text="よい")),
@@ -52,7 +51,7 @@ def lambda_handler(event, context):
                 )
             )
         elif message == 'ダメ':
-            LINE_BOT_API.reply_message(line_event.reply_token,
+            LINE_BOT_API.reply_message(line_reply_event.reply_token,
                 TextSendMessage(text='第2問目\n会ったことのないインターネットのお友達がお金を貸してほしいと聞いてきたら、君は貸す？貸さない？',
                         quick_reply=QuickReply(items=[
                             QuickReplyButton(action=PostbackAction(label="貸す", data="貸す", text="貸す")),
@@ -61,7 +60,7 @@ def lambda_handler(event, context):
                 )
             )
         elif message == '貸さない':
-            LINE_BOT_API.reply_message(line_event.reply_token,
+            LINE_BOT_API.reply_message(line_reply_event.reply_token,
                 TextSendMessage(text='第3問目\nインターネット上でずっと読みたかった漫画が全巻投稿されているのを発見した。\n君は保存する？しない？',
                         quick_reply=QuickReply(items=[
                             QuickReplyButton(action=PostbackAction(label="保存する", data="保存する", text="保存する")),
@@ -70,17 +69,17 @@ def lambda_handler(event, context):
                 )
             )
         elif message in answerlist:
-            LINE_BOT_API.reply_message(line_event.reply_token,
+            LINE_BOT_API.reply_message(line_reply_event.reply_token,
                 TextSendMessage(text='不正解！',
                         quick_reply=QuickReply(items=[
                             QuickReplyButton(action=PostbackAction(label="リトライ", data="リトライ", text="スタート")),
                         ])))
         elif message == '保存しない':
-            LINE_BOT_API.reply_message(line_event.reply_token, StickerSendMessage(package_id='8515',sticker_id='16581254'))
-            LINE_BOT_API.reply_message(line_event.reply_token, TextSendMessage(text='全問正解だ!\nおめでとう!!!'))
+            LINE_BOT_API.reply_message(line_reply_event.reply_token, StickerSendMessage(package_id='8515',sticker_id='16581254'))
+            LINE_BOT_API.reply_message(line_reply_event.reply_token, TextSendMessage(text='全問正解だ!\nおめでとう!!!'))
 
         else:
-            LINE_BOT_API.reply_message(line_event.reply_token, StickerSendMessage(package_id='11537',sticker_id='52002744'))
+            LINE_BOT_API.reply_message(line_reply_event.reply_token, StickerSendMessage(package_id='11537',sticker_id='52002744'))
             return
 
     LINE_HANDLER.handle(body, signature)
